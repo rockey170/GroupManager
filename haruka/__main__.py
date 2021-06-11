42540ee1001920d350c153b427b2c455abac5ce4
rockey170 <81092571+rockey170@users.noreply.github.com>
2021-06-11 12:33:45 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -24,22 +24,22 @@ PM_START = """Hello {}, my name is {}!
 
 You know how hard it is sometimes to manage group so here is the solution for you
 
-I'm group manager bot and Anti-spam for RR Players🃏
+I'm group manager bot and Anti-spam for mr Radical s fans
 
-Activate Anti-spam protection in your BM by this command : 
+Activate Anti-spam protection in your pm by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+bot Owner [Radical](t.me/Radical_official_1)
 
-Special thanks to [this person](t.me/denver02) for helping me.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
 
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
+
+
+
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/ueydydbot?startgroup=true).
 """
 
 
